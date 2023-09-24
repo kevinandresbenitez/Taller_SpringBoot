41be16c67ab0e7f2c55c2e2712a29e0c4002a290
kevinBenitez <kevinescudoderoble@gmail.com>
2023-09-24 20:37:10 -0300

    Eliminando controladores

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -4,16 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-//import com.example.tests.EnfermeroTest;
-//import com.example.tests.FuncionarioTest;
+
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
-      //EnfermeroTest.probarCreacionEnfermeros();
-      //FuncionarioTest.probarCreacionFuncionario();
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
