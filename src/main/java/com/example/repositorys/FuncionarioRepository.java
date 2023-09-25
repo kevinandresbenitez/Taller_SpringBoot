2223c9ce028c0c688e75e7233eb4f8c0488e2793
kevinBenitez <kevinescudoderoble@gmail.com>
2023-09-24 23:00:47 -0300

    Agregardo ServiciosFuncionario,RepositoryFuncionario

---
+++ src/main/java/com/example/repositorys/FuncionarioRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.repositorys;
+
+/**
+ *
+ * @author kevin
+ */
+import com.example.models.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+   
+}
---
+++ src/main/java/com/example/services/FuncionarioService.java
@@ -0,0 +1,30 @@
+package com.example.services;
+
+
+import com.example.repositorys.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import com.example.models.Funcionario;
+
+@Service
+public class FuncionarioService {
+
+    private final FuncionarioRepository funcionarioRepository;
+
+    @Autowired
+    public FuncionarioService(FuncionarioRepository funcionarioRepository) {
+        this.funcionarioRepository = funcionarioRepository;
+    }
+    
+    public void crearFuncionario(Funcionario fun1){
+        this.funcionarioRepository.save(fun1);
+    }
+    
+    public List<Funcionario> obtenerFuncionarios(){
+        return this.funcionarioRepository.findAll();
+    }
+
+}
