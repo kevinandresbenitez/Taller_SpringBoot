a9966558bc8ee6d050cd70c21756225bcd73c389
momet0 <eduu30rb@gmail.com>
2023-09-25 16:28:37 -0300

    probando los controladores

--- src/main/java/com/example/controllers/FuncionarioController.java
+++ src/main/java/com/example/controllers/FuncionarioController.java
@@ -12,6 +12,7 @@ package com.example.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 
