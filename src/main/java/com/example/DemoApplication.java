b6986ee37edc1bdf464b23345acb8ed01838bac6
kevinBenitez <kevinescudoderoble@gmail.com>
2023-09-25 11:15:26 -0300

    Agregardo Controlador funcionarios

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,7 +3,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @EnableJpaRepositories(basePackages = {"pl.hypeapp.episodie.dataproviders"})
 @EntityScan(basePackages = {"pl.hypeapp.core.entity"})
-@SpringBootApplication(scanBasePackages = {"boot.registration"} , exclude = JpaRepositoriesAutoConfiguration.class)
+@SpringBootApplication(scanBasePackages = {"boot.registration","com.example.controllers"} , exclude = JpaRepositoriesAutoConfiguration.class)
 @RestController
 public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+    
+    @GetMapping("/")
+    public String hello(){
+        return "Hello";
     }
 }
---
+++ src/main/java/com/example/controllers/FuncionarioController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.controllers;
+
+/**
+ *
+ * @author kevin
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/funcionario")
+public class FuncionarioController {
+    
+    @GetMapping("/main")
+    public String main(){
+        return String.format("Estas en la pagina de funcionarios");
+    }
+}
