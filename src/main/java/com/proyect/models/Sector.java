dfec36cd59d08c18a7b8b4ff7b76492a379a82b6
kevinBenitez <kevinescudoderoble@gmail.com>
2023-09-27 16:31:22 -0300

    Agregando Sector

---
+++ src/main/java/com/proyect/models/Sector.java
@@ -0,0 +1,21 @@
+package com.proyect.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "sector")
+public class Sector {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name="secto_trabajo")
+    private String sectorTrabajo;
+    @ManyToOne
+    @JoinColumn(name="id_funcionario")
+    private Funcionario funcionario;
+}
