e51e9c3f6319d1068bc29a5839a6cbd63bbdcf6e
momet0 <eduu30rb@gmail.com>
2023-10-06 16:05:10 -0300

    se agregaron atributos a Consulta

--- src/main/java/com/proyect/models/Consulta.java
+++ src/main/java/com/proyect/models/Consulta.java
@@ -4,10 +4,34 @@
  */
 package com.proyect.models;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author ulise
  */
+@Entity
+@Table(name="consulta")
 public class Consulta {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+    @Column(name="fecha_atencion")
+    private Date fechaAtencion;
+    @Column(name="hora_atencion")
+    private Date horaAtencion;
+    @Column(name="diagnostico")
+    private String diagnostico;
+    @Column(name="tipo_atencion")
+    private String tipoAtencion;
+    @Column(name="diagnosticos_clinicos")
+    private String diagnosticosClinicos;
+    @OneToMany(mappedBy = "consulta")
+    private List<ResultadoEstudio> resultadosEstudios;
+
+
 }
