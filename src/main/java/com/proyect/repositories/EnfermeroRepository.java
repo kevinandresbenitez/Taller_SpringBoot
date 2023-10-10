9dd6bcafc71651d3371f8b6b78a3145c14014527
kevinBenitez <kevinescudoderoble@gmail.com>
2023-10-10 17:24:21 -0300

    Agregando Respositorios de medico y enfermero

---
+++ src/main/java/com/proyect/repositories/EnfermeroRepository.java
@@ -0,0 +1,7 @@
+package com.proyect.repositories;
+
+import com.proyect.models.Enfermero;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnfermeroRepository extends JpaRepository<Enfermero,Long> {
+}
---
+++ src/main/java/com/proyect/repositories/MedicoRepository.java
@@ -0,0 +1,7 @@
+package com.proyect.repositories;
+
+import com.proyect.models.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico,Long> {
+}
