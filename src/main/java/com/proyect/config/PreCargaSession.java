7ac8f124bf762627c8c87eeda99296cbf2e02259
kevinBenitez <kevinescudoderoble@gmail.com>
2023-10-22 21:44:45 -0300

    Agregando session

---
+++ src/main/java/com/proyect/config/PreCargaSession.java
@@ -0,0 +1,24 @@
+
+package com.proyect.config;
+import com.proyect.session.SessionUsuario;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class PreCargaSession implements HandlerInterceptor {
+
+    @Autowired
+    private SessionUsuario session;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        if (modelAndView != null) {
+            modelAndView.addObject("sessionUser", session);
+        }
+    }
+
+}
